74d0aaa35d7821120d4e92d7575ae7fae9166c49
Loris Zinsou <loris.zinsou@spendesk.com>
2019-08-13 17:33:02 +0200

    Don't uglify on build (yarn build)

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,12 @@
 import typescript from 'rollup-plugin-typescript2';
-import { uglify } from "rollup-plugin-uglify";
 
 export default {
     input: 'lib/sepa.ts',
     output: {
-        file: 'lib/sepa.min.js',
+        file: 'lib/sepa.js',
         format: 'cjs'
     },
     plugins: [
-        typescript(),
-        uglify()
+        typescript()
     ]
 };
